d616100ffc1dc0898af43a902a8312c9fc38f2ee
daniilseliverstov <selivrstov91@internet.ru>
2025-05-06 16:19:55 +0300

    create Order model. Needs some work

--- orders_manager/orders/models.py
+++ orders_manager/orders/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+
+ORDER_STATUS = (
+    ("accepted", "Принят"),
+    ("in_progress", "В работе"),
+    ("documents", "Документы"),
+    ("suspended", "Приостановлен"),
+    ("reschedule", "Перенос"),
+    ("ready", "Готово"),
+)
+
+
+class Order(models.Model):
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+    order_number = models.CharField(max_length=20, verbose_name="Номер заказа")
+    status = models.CharField(
+        choices=ORDER_STATUS, default="accepted", verbose_name="Статус заказа"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Изменен")
+    month = models.CharField(max_length=10, verbose_name="Месяц", blank=True)
+    week = models.IntegerField(verbose_name="Неделя", blank=True)
+    manager = models.CharField(
+        max_length=20,
+        verbose_name="Менеджер. Создать отдельную модель в приложении Пользователей",
+    )
+    customer = models.CharField(
+        max_length=20,
+        verbose_name="Заказчик. Создать отдельную модель в приложении Пользователей",
+    )
+    technologist = models.CharField(
+        max_length=20,
+        verbose_name="Технолог. Создать отдельную модель в приложении Пользователей",
+        blank=True,
+    )
+    weight = models.DecimalField(
+        max_digits=5, decimal_places=2, verbose_name="Масса", default=0
+    )
+    package = models.IntegerField(
+        verbose_name="Количество упаковок", blank=True, default=0
+    )
+
+    mdf = models.BooleanField(verbose_name="МДФ", default=False)
+    furniture = models.BooleanField(verbose_name="Фурнитура", default=False)
+    grass = models.BooleanField(verbose_name="Стекло", default=False)
+    cnc = models.BooleanField(verbose_name="ЧПУ", default=False)
+
+    materials = models.CharField(
+        verbose_name="Материалы. Создать отдельную модель", blank=True
+    )
+
+    quadrature = models.DecimalField(
+        max_digits=5, decimal_places=2, verbose_name="Квадратура", blank=True, default=0
+    )
+    serial = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        verbose_name="Серийная продукция",
+        blank=True,
+        default=0,
+    )
+    portal = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        verbose_name="Каминные порталы",
+        blank=True,
+        default=0,
+    )
+    reclamation = models.CharField(
+        max_length=250, verbose_name="Причина рекламации", blank=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.order_number}"
